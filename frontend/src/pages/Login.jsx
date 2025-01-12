3a7ba8f5cb521799c674cd55689d0a9b00e3392a
NibidaGhimire <nibidaghimire39@gmail.com>
2025-01-12 20:37:56 +0545

    add login,signup ui

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,92 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useLogin from "../hooks/useLogin";
+import { logo } from "../assets";
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const { loading, login } = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  };
+
+  return (
+    <div className="flex flex-row items-center justify-center">
+      <div className=" flex-[40%] flex flex-col bg-bg bg-opacity-70 justify-center items-center h-screen w-fit p-20 shadow-lg shadow-gray-200 rounded-box">
+        <img src={logo} alt="PhysioNep" className="w-96 h-96 object-cover " />
+        <br />
+        <p className="text-[24px] text-black font-bold">
+          {" "}
+          Welcome to <span className="text-red-500">Mokshya!</span>
+        </p>
+      </div>
+      <div className="flex-[60%] p-4 h-screen flex items-center justify-center ">
+        <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+          <div className="w-full p-6 rounded-lg shadow-md outline  bg-bg bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-60">
+            <h1 className="text-3xl font-semibold text-center text-gray-800">
+              Login
+            </h1>
+
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label className="label p-2">
+                  <span className="text-base label-text text-gray-800">
+                    Username
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Username"
+                  className="w-full input input-bordered h-10 bg-transparent text-gray-800"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label className="label p-2 ">
+                  <span className="text-base label-text text-gray-800">
+                    Password
+                  </span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  className="w-full input input-bordered h-10 bg-transparent text-gray-800"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              <Link
+                to="/signup"
+                className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block text-gray-800"
+              >
+                {"Don't"} have an account?
+              </Link>
+
+              <div>
+                <button
+                  disabled={loading}
+                  className="btn btn-block btn-sm mt-2 bg-black hover:outline hover:bg-primarygreen/30 text-white"
+                >
+                  {loading ? (
+                    <span className="loading loading-spinner"></span>
+                  ) : (
+                    "Login"
+                  )}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,10 +1,146 @@
+import { Link } from "react-router-dom";
+import GenderCheckbox from "../components/GenderCheckbox";
+import { useState } from "react";
+import useSignup from "../../src/hooks/useSignup";
+import { logo } from "../assets";
 
-const Signup = () => {
+const SignUp = () => {
+  const [inputs, setInputs] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+
+  const { loading, signup } = useSignup();
+
+  const handleCheckboxChange = (gender) => {
+    setInputs({ ...inputs, gender });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signup(inputs);
+  };
+
   return (
-    <div>
-      Signup
-    </div>
-  )
-}
+    <div className="flex flex-row items-center justify-center">
+      <div className=" flex-[40%] flex flex-col bg-bg bg-opacity-70 justify-center items-center h-screen w-fit p-20 shadow-lg shadow-gray-200 rounded-box">
+        <img src={logo} alt="PhysioNep" className="w-96 h-96 object-cover " />
+        <br />
+        <p className="text-[24px] text-black font-bold">
+          {" "}
+          Welcome to <span className="text-red-500">Mokshya!</span>
+        </p>
+      </div>
 
-export default Signup
+      <div className="flex-[60%] p-4 h-screen flex items-center justify-center ">
+        <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+          <div className="w-full p-6 rounded-lg shadow-md bg-bg outline bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-60">
+            <h1 className="text-3xl font-semibold text-center text-gray-800">
+              Sign Up
+            </h1>
+
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label className="label p-2">
+                  <span className="text-base label-text text-gray-800">
+                    Full Name
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Jane Doe"
+                  className="w-full input input-bordered h-10 bg-transparent text-gray-800"
+                  value={inputs.fullName}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, fullName: e.target.value })
+                  }
+                />
+              </div>
+
+              <div>
+                <label className="label p-2">
+                  <span className="text-base label-text  text-gray-800">
+                    Username
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="janedoe"
+                  className="w-full input input-bordered h-10 bg-transparent  text-gray-800"
+                  value={inputs.username}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, username: e.target.value })
+                  }
+                />
+              </div>
+
+              <div>
+                <label className="label">
+                  <span className="text-base label-text  text-gray-800">
+                    Password
+                  </span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  className="w-full input input-bordered h-10 bg-transparent"
+                  value={inputs.password}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, password: e.target.value })
+                  }
+                />
+              </div>
+
+              <div>
+                <label className="label">
+                  <span className="text-base label-text text-gray-800">
+                    Confirm Password
+                  </span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  className="w-full input input-bordered h-10 bg-transparent"
+                  value={inputs.confirmPassword}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, confirmPassword: e.target.value })
+                  }
+                />
+              </div>
+
+              <GenderCheckbox
+                onCheckboxChange={handleCheckboxChange}
+                selectedGender={inputs.gender}
+              />
+
+              <Link
+                to="/login"
+                className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block text-gray-800"
+              >
+                Already have an account?
+              </Link>
+
+              <div>
+                <button
+                  disabled={loading}
+                  className="btn btn-block btn-sm mt-2 border text-white border-slate-700  hover:outline hover:bg-primarygreen/30"
+                >
+                  {loading ? (
+                    <span className="loading loading-spinner"></span>
+                  ) : (
+                    "Sign Up"
+                  )}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
